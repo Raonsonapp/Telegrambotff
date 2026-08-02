f75d5dbe32c477d55b49766a8e842df1d391a4c0
ChatApp Messenger <shahronmahmadmurodzoda08@gmail.com>
2026-08-02 18:53:30 +0500

    Remove entering_custom_amount state from OrderFlow

--- bot/states.py
+++ bot/states.py
@@ -4,7 +4,6 @@ from aiogram.fsm.state import State, StatesGroup
 class OrderFlow(StatesGroup):
     choosing_product = State()
     choosing_cart = State()
-    entering_custom_amount = State()
     entering_player_id = State()
     confirming = State()
     awaiting_payment_proof = State()
