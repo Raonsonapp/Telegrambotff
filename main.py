2fccccb4968e2c14223ba45b21330bdc510c26c7
ChatApp Messenger <shahronmahmadmurodzoda08@gmail.com>
2026-08-09 00:13:55 +0500

    Update main.py

--- main.py
+++ main.py
@@ -91,7 +91,7 @@ async def run_webhook(bot: Bot, dp: Dispatcher) -> None:
         drop_pending_updates=True,
     )
 
-       info = await bot.get_webhook_info()
+    info = await bot.get_webhook_info()
     logging.info("TELEGRAM WEBHOOK URL: %s", info.url)
     logging.info("TELEGRAM PENDING UPDATES: %s", info.pending_update_count)
     logging.info("TELEGRAM LAST ERROR: %s", info.last_error_message)
